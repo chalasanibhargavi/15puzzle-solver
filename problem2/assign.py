df9a397bc56e827ad07271f18dc3c406ea5ccfb9
Ankita Rajendra Alshi <aralshi@silo.soic.indiana.edu>
2017-09-29 10:14:22 -0400

    Assignment 1 question 2 with comments

--- problem2/assign.py
+++ problem2/assign.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-# a0.py : Solve the N-Queens and N-Rooks problem!
-# Ankita Alshi, 2017
+# assign.py : Given n number of students with their preferences, assign them to teams such that overall time for
+#             grading the assignment is minimum.
+# Team - Ankita Alshi, Bhargavi Chalasa, Dheeraj Singh, 2017
+#
+# Comments:
 
-# Input from command line: 1. input file1                     2. Time taken to grade one team 
-#                          3. time taken to answer mail query 4. time for a meeting to resolve issue
 
 import sys
 import datetime
 
+# Create result string to print the final teams in required output format
 def print_sol(board):
     result = ""
     for row in board:
@@ -18,6 +20,7 @@ def print_sol(board):
             result += "\n"
     return result
 
+# To check if adding student to team is cost effective (check for conflict of interest)
 def iscosteff(student, team):
     indexi = listuserid.index(student)
     for mem in team:
@@ -26,12 +29,14 @@ def iscosteff(student, team):
             return False
     return True
 
+# Remove student from ith team and add him to nth team
 def add_student(board, n, i, student):
     s = [[val for val in row] for row in board]
     s[n].append(student)
     s[i].remove(student)
     return s
 
+# Calculate number of students assign to team size other than their preference
 def wrong_team_size(team):
     count = 0
     for mem in team:
@@ -41,6 +46,7 @@ def wrong_team_size(team):
                     count += 1
     return count
 
+# Calculate number of students who did not get team members of their choice
 def notgotwanted(team):
     count = 0
     for memi in team:
@@ -52,6 +58,7 @@ def notgotwanted(team):
                         count += 1
     return count
 
+# Calculate number of student who got team member that they did not wanted to work with
 def gotunwanted(team):
     count = 0
     for memi in team:
@@ -62,11 +69,13 @@ def gotunwanted(team):
                         count += 1
     return count
 
+# Define initial team arrangement as one student per team
 def initial_board():
     for student in listuserid:
         initial.append([student])
     return [val for val in initial]
 
+# Find the successor with lowest cost
 def getmin(board_list):
     for x in range(len(board_list)):
         if (x == 0):
@@ -79,6 +88,7 @@ def getmin(board_list):
                 minindex = x
     return board_list[minindex]
 
+# Calculate cost for a team
 def cost_team(team):
     if (len(team) == 0):
         return 0
@@ -89,6 +99,7 @@ def cost_team(team):
         cost = k + (1 * number_wrongteamnum) + (n * number_notgotwanted) + (m * number_gotunwanted)
     return cost
 
+# Calculate total cost for given arrangement of teams
 def tot_cost(board):
     total_cost = 0
     for row in board:
@@ -96,6 +107,7 @@ def tot_cost(board):
             total_cost += cost_team(row)
     return total_cost
 
+# Create successors by adding students to different teams
 def successor(board, i):
     move_student = board[i][0]
     succ = []
@@ -105,7 +117,8 @@ def successor(board, i):
             if (curr_size > 0 and curr_size < 3 and iscosteff(move_student, board[n])):
                 succ.append(add_student(board, n, i, move_student))                          
     return succ
-        
+
+# Solve the problem        
 def solve():
     fringe = [initial_board()]
     s = getmin(fringe)
@@ -123,47 +136,54 @@ def solve():
         fringe = [s]
     return s
 
+# Store student preference to work with each other. (used to calculate the cost of a team) 
+def storepref():
+    for i in range(0, len(listuserid)):
+        for j in range(0, len(listuserid)):
+            if (i == j):
+                mem_pref[i][j] += 1
+            else:
+                if (listuserid[j] in listdict[i]['unwantedmem']):
+                    mem_pref[i][j] = -1
+                elif (listuserid[j] in listdict[i]['wantedmem']):
+                    mem_pref[i][j] += 1
 
+# Storin input data from file to dictionary
+def filetodict():
+    for a in ifile:
+        line = a.split()
+        d = {'userid': line[0], 'teamnum': line[1], 'wantedmem': line[2].split(","), 'unwantedmem': line[3].split(",")}
+        if (line[2] == "_"):
+            d['wantedmem'] = []
+        if (line[3] == "_"):
+            d['unwantedmem'] = []
+        listdict.append(d)
+    return
+
+# Input from command line: 1. input file1                     2. Time taken to grade one team 
+#                          3. time taken to answer mail query 4. time for a meeting to resolve issue
 filename = sys.argv[1]
 k = int(sys.argv[2])
 n = int(sys.argv[3]) 
 m = int(sys.argv[4])
 
+# Open input file containing student userid with their preferences
 ifile = open(filename, "r")
+
+# List of Dictionaries to store input values
 listdict = []
-initial = []
+filetodict()
 
-for a in ifile:
-    line = a.split()
-    d = {'userid': line[0], 'teamnum': line[1], 'wantedmem': line[2].split(","), 'unwantedmem': line[3].split(",")}
-    if (line[2] == "_"):
-        d['wantedmem'] = []
-    if (line[3] == "_"):
-        d['unwantedmem'] = []
-    listdict.append(d)
-
-rows = cols = len(listdict)
-mem_pref = [[0 for j in range(cols)] for i in range(rows)]
-
+# List of userids given
 listuserid = [a['userid'] for a in listdict]
 
-for i in range(0, len(listuserid)):
-    for j in range(0, len(listuserid)):
-        if (i == j):
-            mem_pref[i][j] += 1
-        else:
-            if (listuserid[j] in listdict[i]['unwantedmem']):
-                mem_pref[i][j] = -1
-            elif (listuserid[j] in listdict[i]['wantedmem']):
-                mem_pref[i][j] += 1
+# Student preference stored in N*N list (where N is number of userids)
+rows = cols = len(listdict)
+mem_pref = [[0 for j in range(cols)] for i in range(rows)]
+storepref()
 
+# Solve the the problem and print final list of team with the time required to grade them.
+initial = []
 goal = solve()
 print (print_sol(goal))
-print (tot_cost(goal))
-
-
-
-
-            
-                    
- 
+print (tot_cost(goal)) 
