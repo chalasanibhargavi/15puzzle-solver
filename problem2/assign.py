913693a420662b2aba0e4833349a1eb70c0a430a
Ankita Rajendra Alshi <aralshi@tank.soic.indiana.edu>
2017-10-01 20:20:07 -0400

    question 2 heapqueue implementation

--- problem2/assign.py
+++ problem2/assign.py
@@ -5,7 +5,7 @@
 #
 # Comments:
 
-
+from heapq import *
 import sys
 import datetime
 
@@ -69,30 +69,14 @@ def gotunwanted(team):
 def initial_board():
     for student in listuserid:
         initial.append([student])
-    return [val for val in initial]
-
-# Find the successor with lowest cost
-def getmin(board_list):
-    for x in range(len(board_list)):
-        if (x == 0):
-            mincost = tot_cost(board_list[x])
-            minindex = x
-        else:
-            curr_cost = tot_cost(board_list[x])
-            if (curr_cost < mincost):
-                mincost = tot_cost(board_list[x])
-                minindex = x
-    return board_list[minindex]
+    return initial
 
 # Calculate cost for a team
 def cost_team(team):
     if (len(team) == 0):
         return 0
     else:
-        number_wrongteamnum = wrong_team_size(team)
-        number_notgotwanted = notgotwanted(team)
-        number_gotunwanted = gotunwanted(team)
-        cost = k + (1 * number_wrongteamnum) + (n * number_notgotwanted) + (m * number_gotunwanted)
+        cost = k + (1 * wrong_team_size(team)) + (n * notgotwanted(team)) + (m * gotunwanted(team))
     return cost
 
 # Calculate total cost for given arrangement of teams
@@ -112,41 +96,42 @@ def successor(board, i):
             curr_size = len(board[n])
             if (curr_size > 0 and curr_size < 3):
                  temp = add_student(board, n, i, move_student)
-                 if (tot_cost(temp) <= curr_cost):
-                     succ.append(temp)
+                 succ_cost = tot_cost(temp)
+                 if (succ_cost <= curr_cost):
+                     succ.append([succ_cost, temp])
     return succ
 
 # Solve the problem recursively till all the team members of ith team are assigned to other teams
 def recur_solve(s, i):
     if(not len(s[i]) == 0):
         for x in successor(s, i):
-            fringe.append(x)
-            recur_solve(x, i)
+            heappush(fringe, (x[0], x[1]))
+            recur_solve(x[1], i)
     return
 
 # Solve the problem by adding team member from ith team to other teams to find minimum cost
 def solve():
     global fringe, curr_cost
-    fringe = [initial_board()]
-    s = getmin(fringe)
+    smin = []
+    fringe = []
+    s = initial_board()
+    curr_cost = tot_cost(s)
+    heappush(fringe, (curr_cost, s))
     for i in range(rows):
-        curr_cost = tot_cost(s)
-        recur_solve(s, i)
-        s = getmin(fringe)
-        fringe = [s]
+        if (i == 0):
+            recur_solve(s, i)
+        else:
+            for each in smin:
+                curr_cost = each[0]
+                s = each[1]
+                heappush(fringe, (curr_cost, s))
+                recur_solve(s, i)
+                
+        if (i == rows-1):
+            curr_cost, s = heappop(fringe)
+        else:
+            smin = nsmallest(nsmall, fringe)
     return s 
-
-# Store student preference to work with each other. (used to calculate the cost of a team) 
-def storepref():
-    for i in range(0, len(listuserid)):
-        for j in range(0, len(listuserid)):
-            if (i == j):
-                mem_pref[i][j] += 1
-            else:
-                if (listuserid[j] in listdict[i]['unwantedmem']):
-                    mem_pref[i][j] = -1
-                elif (listuserid[j] in listdict[i]['wantedmem']):
-                    mem_pref[i][j] += 1
 
 # Storin input data from file to dictionary
 def filetodict():
@@ -176,14 +161,14 @@ filetodict()
 
 # List of userids given
 listuserid = [a['userid'] for a in listdict]
-
-# Student preference stored in N*N list (where N is number of userids)
 rows = cols = len(listdict)
-mem_pref = [[0 for j in range(cols)] for i in range(rows)]
-storepref()
+if (rows > 50):
+    nsmall = 1
+else:
+    nsmall = 2
 
 # Solve the the problem and print final list of team with the time required to grade them.
 initial = []
 goal = solve()
 print (print_sol(trim(goal)))
-print (tot_cost(goal)) 
+print (tot_cost(goal))
